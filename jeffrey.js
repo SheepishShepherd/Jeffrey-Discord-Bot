3fac3bd48b6073aadc389d75192223f45734032a
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-03 02:37:39 -0500

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var rolld6 = Math.floor(Math.random() * 6) + 1;
 
 client.on('ready', () => {console.log('Jeffrey is ready to play!');});
 
@@ -10,7 +11,13 @@ client.on('message', message => {
   
   else if(message.content === '/help'){message.reply('Go to the Help Channel ya doofus!');}
   
-
+  else if(message.content === '/roll d6'){
+    if(rolld6 = 1){files: [/Images/GameProps/D6/d6_1.jpg]}
+    if(rolld6 = 2){files: [/Images/GameProps/D6/d6_2.jpg]}
+    if(rolld6 = 3){files: [/Images/GameProps/D6/d6_3.jpg]}
+    if(rolld6 = 4){files: [/Images/GameProps/D6/d6_4.jpg]}
+    if(rolld6 = 5){files: [/Images/GameProps/D6/d6_5.jpg]}
+    if(rolld6 = 6){files: [/Images/GameProps/D6/d6_6.jpg]};}
   
   else {message.reply("That's not a command, silly!").then(msg => {msg.delete(10000)})
     .catch(/*Your Error handling if the Message isn't returned, sent, etc.*/);}
