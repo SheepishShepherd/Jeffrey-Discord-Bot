5ab15ce9d1c4a612ba516f91168855ca8c62d2da
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-03 01:36:40 -0500

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {console.log('Jeffrey is ready to play!');});
 
 client.on('message', message => {
-  if(message.content.indexOf(config.prefix) !== 0){return;}
+  if(message.content.charAt(0) !== '/'){return;}
   else if(message.content === '/ping'){message.reply('PONG! :ping_pong:');}
   else if(message.content === '/help'){message.reply('Go to the Help Channel ya doofus!');}
   else {message.reply("That's not a command, silly!");}
