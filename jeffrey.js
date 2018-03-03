3cb6c77bb0b3d9854ddbb84092c3d1b84ec055c7
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-03 13:03:02 -0500

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -12,12 +12,12 @@ client.on('message', message => {
   else if(message.content === '/help'){message.reply('Go to the Help Channel ya doofus!');}
   
   else if(message.content === '/roll'){
-         if(roll6 = 1){message.reply('You rolled a 1!');}
-    else if(roll6 = 2){message.reply('You rolled a 2!');}
-    else if(roll6 = 3){message.reply('You rolled a 3!');}
-    else if(roll6 = 4){message.reply('You rolled a 4!');}
-    else if(roll6 = 5){message.reply('You rolled a 5!');}
-    else if(roll6 = 6){message.reply('You rolled a 6!');}
+         if(rolld6 = 1){message.reply('You rolled a 1!');}
+    else if(rolld6 = 2){message.reply('You rolled a 2!');}
+    else if(rolld6 = 3){message.reply('You rolled a 3!');}
+    else if(rolld6 = 4){message.reply('You rolled a 4!');}
+    else if(rolld6 = 5){message.reply('You rolled a 5!');}
+    else if(rolld6 = 6){message.reply('You rolled a 6!');}
   }
   
   else {message.reply("That's not a command, silly!").then(msg => {msg.delete(10000)})
