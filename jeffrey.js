11b3f46f62b7d0f28db4a5df71e436bdd19a19ea
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-03 14:00:10 -0500

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -8,17 +8,19 @@ client.on('message', message => {
   
   else if(message.content === '/ping'){message.reply('PONG! :ping_pong:');}
   
-  else if(message.content === '/help'){message.channel.send('Go to the Help Channel ya doofus!');}
+  else if(message.content === '/help'){message.channel.send('If you need help with commands, go to #jeffrey-commands');}
   
   else if(message.content === '/roll'){
     var rolld6 = Math.floor((Math.random() * 6) + 1);
-         if(rolld6 == 1){message.reply('You rolled a 1!');}
-    else if(rolld6 == 2){message.reply('You rolled a 2!');}
-    else if(rolld6 == 3){message.reply('You rolled a 3!');}
-    else if(rolld6 == 4){message.reply('You rolled a 4!');}
-    else if(rolld6 == 5){message.reply('You rolled a 5!');}
-    else if(rolld6 == 6){message.reply('You rolled a 6!');}
+         if(rolld6 == 1){message.reply('has rolled a 1!');}
+    else if(rolld6 == 2){message.reply('has rolled a 2!');}
+    else if(rolld6 == 3){message.reply('has rolled a 3!');}
+    else if(rolld6 == 4){message.reply('has rolled a 4!');}
+    else if(rolld6 == 5){message.reply('has rolled a 5!');}
+    else if(rolld6 == 6){message.reply('has rolled a 6!');}
   }
+  
+  ekse if(message.content === '/pet'){message.channel.send({files: ["/images/jeffrey/cute.jpg"]});}
   
   else {message.reply("That's not a command, silly!").then(msg => {msg.delete(10000)})
     .catch(/*Your Error handling if the Message isn't returned, sent, etc.*/);}
