692f99fa8f66fd5af1d50b8af06d0a9b534ecc5d
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-02 23:40:17 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {console.log('Jeffrey is ready to play!');});
+client.on('message', message => {if(message.content === 'ping'){message.reply('PONG! :ping_pong:');}});
+
+client.login(process.env.BOT_TOKEN);
