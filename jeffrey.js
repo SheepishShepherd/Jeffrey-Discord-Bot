5743062112a36c0fbbd83e082a92344a33ee9082
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-02 23:58:31 -0500

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {console.log('Jeffrey is ready to play!');});
-client.on('message', message => {if(message.content === 'ping'){message.reply('PONG! :ping_pong:');}});
+client.on('message', message => {if(message.content === '/ping'){message.reply('PONG! :ping_pong:');}});
+client.on('message', message => {if(message.content === '/help'){message.reply('Go to the Help Channel ya doofus!');}});
 
 client.login(process.env.BOT_TOKEN);
