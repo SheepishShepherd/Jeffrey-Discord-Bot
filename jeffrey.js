c906bb7eb2ae48d01e007b724c7068ff6d4a9f58
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-03 01:31:28 -0500

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -7,8 +7,7 @@ client.on('message', message => {
   if(message.content.indexOf(config.prefix) !== 0){return;}
   else if(message.content === '/ping'){message.reply('PONG! :ping_pong:');}
   else if(message.content === '/help'){message.reply('Go to the Help Channel ya doofus!');}
-  else {message.reply("That's not a command, silly!") .then(msg => {msg.delete(10000)})
-  .catch(/*Your Error handling if the Message isn't returned, sent, etc.*/);}
+  else {message.reply("That's not a command, silly!");}
 });
 
 client.login(process.env.BOT_TOKEN);
