1ac28efdd95d853162366139e1e82a5a29d996c2
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-19 00:08:15 -0400

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -11,8 +11,7 @@
     
     var args = message.content.substring(prefix.length).split(" ");
     
-    switch (args[0].toLowerCase()) {
-    
+    switch (args[0].toLowerCase()) {   
         case "ping":
           message.reply('PONG! :ping_pong:');
           break;
@@ -29,14 +28,14 @@
           else if(args[1]){
             if(args[1] == "6"){message.channel.sendMessage("You rolled a six-sided die!");}
             else if(args[1] == "20"){message.channel.sendMessage("You rolled a twenty-sided die!");}
-            else{message.channel.sendMessage("That's not a valid number!");}
-          }
+            else{message.channel.sendMessage("That's not a valid number!");}}
         
-          else{message.channel.sendMessage("I'm a little confused...");}
+          else{return;}
           break;
+//Rolls a die(dice) based on inputs of the user.
         
         default:
-          message.channel.sendMessage("That's not a command, silly!");
+          message.channel.sendMessage("That's not a command, silly!").then(message.delete(10000););
 //If the user tries to run a command that does not exist, the bot will state so
     }
   });
