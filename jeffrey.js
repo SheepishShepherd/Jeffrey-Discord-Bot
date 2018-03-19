71a657066dc66145536485c89df408183dea0e36
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-18 21:37:11 -0400

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -1,31 +1,32 @@
   const Discord = require('discord.js');
-  const client = new Discord.Client();
+  const bot = new Discord.Client();
   const prefix = "/";
 
-  client.on('ready', () => {console.log('Jeffrey is ready to play!');});
+  bot.on('ready', () => {console.log('Jeffrey is ready to play!');});
 
-  client.on('message', message => {
+  bot.on('message', message => {
     if(!message.content.startsWith(prefix)) return;
+    if(message.author.equals(bot.user)) return;
 //If the message does not start with the set prefix, the bot will ignore the message.
     
     var args = message.content.substring(prefix.length).split(" ");
     
-    switch(args[0].toLowerCase()) {
+    switch (args[0].toLowerCase()) {
     
         case "ping":
-        message.reply('PONG! :ping_pong:');
-        break;
+          message.reply('PONG! :ping_pong:');
+          break;
 //Replies "Pong!" if the user uses /ping
 
         case "help":
-        message.channel.sendMessage('If you need help with commands, go to #command-list');
-        break;
+          message.channel.sendMessage('If you need help with commands, go to #command-list');
+          break;
 //Refers the help channel for a command list
         
         default:
-        message.channel.sendMessage("That's not a command, silly!");
+          message.channel.sendMessage("That's not a command, silly!");
 //If the user tries to run a command that does not exist, the bot will state so
     }
   });
     
-  client.login(process.env.BOT_TOKEN);
+  bot.login(process.env.BOT_TOKEN);
