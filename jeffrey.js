28e55528080e6d1bf7799a20ff5dd70d659b07d1
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-18 21:40:05 -0400

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -24,17 +24,8 @@
 //Refers the help channel for a command list
         
         case "roll":
-          if(args[1] = "6"){
-          var rolld6 = Math.floor((Math.random() * 6) + 1);
-          if(rolld6 == 1){message.reply('has rolled a 1!', {file: 'https://raw.githubusercontent.com/SheepishShepherd/Jeffrey-Discord-Bot/master/Images/GameProps/D6/d6_1.png'});}
-          else if(rolld6 == 2){message.reply('has rolled a 2!', {file: 'https://raw.githubusercontent.com/SheepishShepherd/Jeffrey-Discord-Bot/master/Images/GameProps/D6/d6_2.png'});}
-          else if(rolld6 == 3){message.reply('has rolled a 3!', {file: 'https://raw.githubusercontent.com/SheepishShepherd/Jeffrey-Discord-Bot/master/Images/GameProps/D6/d6_3.png'});}
-          else if(rolld6 == 4){message.reply('has rolled a 4!', {file: 'https://raw.githubusercontent.com/SheepishShepherd/Jeffrey-Discord-Bot/master/Images/GameProps/D6/d6_4.png'};}
-          else if(rolld6 == 5){message.reply('has rolled a 5!', {file: 'https://raw.githubusercontent.com/SheepishShepherd/Jeffrey-Discord-Bot/master/Images/GameProps/D6/d6_5.png'});}
-          else if(rolld6 == 6){message.reply('has rolled a 6!', {file: 'https://raw.githubusercontent.com/SheepishShepherd/Jeffrey-Discord-Bot/master/Images/GameProps/D6/d6_6.png'});}}
-          else{message.channel.sendMessage('You need a proper dice number!');}
+          message.channel.sendMessage("What dice do you want to roll?");
           break;
-//User rolls a die
         
         default:
           message.channel.sendMessage("That's not a command, silly!");
