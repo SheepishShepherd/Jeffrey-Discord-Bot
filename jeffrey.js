d920c262ed2dbe63c724d761987294747fb8293d
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-19 06:14:53 -0400

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -25,12 +25,12 @@
         case "roll":
           if(!args[1]){
             var d6roll = Math.floor((Math.random() * 6) + 1);
-            if(d6roll == 1){message.channel.reply("rolled a 1!");}
-            else if(d6roll == 2){message.channel.reply("rolled a 2!");}
-            else if(d6roll == 3){message.channel.reply("rolled a 3!");}
-            else if(d6roll == 4){message.channel.reply("rolled a 4!");}
-            else if(d6roll == 5){message.channel.reply("rolled a 5!");}
-            else if(d6roll == 6){message.channel.reply("rolled a 6!");}
+            if(d6roll == 1){message.reply("rolled a 1!");}
+            else if(d6roll == 2){message.reply("rolled a 2!");}
+            else if(d6roll == 3){message.reply("rolled a 3!");}
+            else if(d6roll == 4){message.reply("rolled a 4!");}
+            else if(d6roll == 5){message.reply("rolled a 5!");}
+            else if(d6roll == 6){message.reply("rolled a 6!");}
             else {message.channel.reply("rolled a six-sided die!");}
           }
 
