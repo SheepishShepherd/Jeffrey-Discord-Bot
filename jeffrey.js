388a30fb7ed5bd9e54b8e5bf11a182e4063b1450
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-18 21:08:28 -0400

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -6,26 +6,7 @@
 
   client.on('message', function(message) {
     if (!message.content.startsWith(prefix)) return;
-//If the message does not start with the set prefix, the bot will ignore the message.
-    
-    var args = message.content.substring(prefix.length).split(" ");
-    
-    switch(args[0].toLowerCase()) {
-    
-        case "ping":
-        message.reply('PONG! :ping_pong:');
-        break;
-//Replies "Pong!" if the user uses /ping
-        
-        case "help":
-        message.channel.sendMessage('If you need help with commands, go to #command-list');
-        break;
-//Refers the help channel for a command list
-
-        default:
-        message.channel.sendMessage("That's not a command, silly!");
-//If the user tries to run a command that does not exist, the bot will state so
-    }
+    else{message.reply('Oh hi!')}
   }
 
   client.login(process.env.BOT_TOKEN);
