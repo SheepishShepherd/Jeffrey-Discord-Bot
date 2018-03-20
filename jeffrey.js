e658262a6fb4ea8c4b3cbdf2e0f250674675790d
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-20 11:21:46 -0400

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -30,7 +30,7 @@
         
         case "profile":
           var prof = new Discord.RichEmbed()
-            .addField(message.author.username, message.author.nickame)
+            .addField(message.author.username, detailsOf(message.author).nick)
             .addField("Member")
             .setColor(0X00FFF)
             .setThumbnail(message.author.avatarURL)
