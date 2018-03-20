9ac7c8e6cbc649e691eba39cedcb52b4120d8ce4
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-20 19:54:30 -0400

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -49,7 +49,7 @@
               else if (message.member.roles.has(roleD.id)) {roleColor = "0xd9b3ff"; roleName = "Sheep Companion";}
               else {roleColor = "0xd9b3ff"; roleName = "Sheep";}
             var prof = new Discord.RichEmbed()
-              .addField(message.author.username, message.author.username.nick)
+              .addField(message.author.username, message.author.username.nickname)
               .setFooter(roleName)
               .setColor(roleColor)
               .setThumbnail(message.author.avatarURL)
