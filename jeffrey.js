6456c972f6683c336d0eb6c5015d296ff6029257
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-20 22:28:01 -0400

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -66,11 +66,24 @@
           }
           //Applied when the user DOES mention another user
           else if(args[1]){
+            var roleA = message.guild.roles.find("name", "The Shepherd");
+            var roleB = message.guild.roles.find("name", "Bot");
+            var roleC = message.guild.roles.find("name", "Shepherd Helpers");
+            var roleD = message.guild.roles.find("name", "Sheep Companions");
+            var roleE = message.guild.roles.find("name", "The Sheep");
+            var roleColor = "";
+            var roleName = "";
+            var userNameP = message.member.username;
+            if (message.member.roles.has(roleA.id)) {roleColor = "0xffaa00"; roleName = "The Shepherd";}
+              else if (message.member.roles.has(roleB.id)) {roleColor = "0xff6464"; roleName = "Bot";}
+              else if (message.member.roles.has(roleC.id)) {roleColor = "0x80b6e5"; roleName = "Sheep Helper";}
+              else if (message.member.roles.has(roleD.id)) {roleColor = "0xd9b3ff"; roleName = "Sheep Companion";}
+              else {roleColor = "0xfffeda"; roleName = "Sheep";}
             var ment = message.mentions.members.first();
             var prof = new Discord.RichEmbed()
-              .addField("username", ment.nickname)
-              .setFooter("Sheep")
-              .setColor(0xfffeda)
+              .addField(userNameP, ment.nickname)
+              .setFooter("roleName")
+              .setColor(roleColor)
               .setThumbnail(ment.avatarURL)
             message.channel.sendEmbed(prof);
           }
