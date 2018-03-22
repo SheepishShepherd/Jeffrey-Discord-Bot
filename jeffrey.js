af3269ad08ffcf4afb6a2a288bf0b3af7edf2e10
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-21 21:09:47 -0400

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -32,7 +32,7 @@
         case "test":
           var ment = message.mentions.members.first();
           if(args[1]){message.channel.send(ment.nickname + " // " + 
-                                           message.mentions.members.username + " // " + 
+                                           message.mentions.username + " // " + 
                                            ment + " // " + 
                                            ment.id + " // " +
                                            message.author.username);}
