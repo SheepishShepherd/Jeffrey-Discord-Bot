ae2d30b7e3745a06b0b029183b0bc63524967c48
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-22 04:37:44 -0400

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -2,6 +2,11 @@
   const bot = new Discord.Client();
   const prefix = "!";
 
+  String[] memberList = new String[3];
+  memberList[0] = "@SheepishShepherd#1247";
+  memberList[1] = "@Cloftus#2645";
+  memberList[2] = "@Jeffrey#5631";
+
   bot.on('ready', () => {console.log('Jeffrey is ready to play!');});
 
 //Gives new users the "Sheep" role
