10d079e85f2c666b0fecf0a9d47154a2d7287fc1
SheepishShepherd <36808587+SheepishShepherd@users.noreply.github.com>
2018-03-22 09:44:01 -0400

    Update jeffrey.js

--- jeffrey.js
+++ jeffrey.js
@@ -92,16 +92,19 @@
         case "roll":
           if(!args[1]){
             var roll = Math.floor((Math.random() * 6) + 1);
-            message.reply("rolled a " + roll + "!");}
+            var rolln = roll.toString();
+            message.reply("rolled a " + rolln + "!");}
 
           else if(args[1]){
             if(args[1] == "6"){
               var d6roll = Math.floor((Math.random() * 6) + 1);
-              message.reply("rolled a " + d6roll + "!");
+              var roll6 = d6roll.toString();
+              message.reply("rolled a " + roll6 + "!");
             
             else if(args[1] == "20"){
               var d20roll = Math.floor((Math.random() * 20) + 1);
-              message.reply("rolled a " + d20roll + "!");
+              var roll20 = d20roll.toString();
+              message.reply("rolled a " + roll20 + "!");
             
             else{message.channel.send("That's not a valid number!");}}
         
